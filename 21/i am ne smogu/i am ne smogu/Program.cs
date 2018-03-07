31fca91d5f31eced2942c25a50b57fe6c4df4a23
NachinauchiyGaibe <superhoroshiy@mail.ru>
2018-03-07 18:09:09 +0300

    I chto- to cdelal

---
+++ 21/i am ne smogu/i am ne smogu/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace i_am_ne_smogu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int schetchik = 0;
+            for (int i = 0; i <20; i++)
+            {
+                
+                Console.WriteLine("Довой пегроем в икру. Нозови свай раст и я...");
+                string g = Console.ReadLine();
+                int Rost_Comrada = int.Parse(g);
+                if (Rost_Comrada > 160)
+                {
+                     Console.WriteLine("Расстрелять");
+                    schetchik++;
+                }
+                else
+                {
+                    Console.WriteLine("Ти пака жив...");
+                }
+                
+
+            }
+            Console.WriteLine(schetchik);
+
+
+
+
+
+        }
+    }
+}
