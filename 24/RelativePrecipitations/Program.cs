6c7535d6552fc43c1442e28226bf5c9ea2c22dd2
Alex <iseldomused@gmail.com>
2018-03-26 01:16:29 +0300

    add 24

---
+++ 24/RelativePrecipitations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RelativePrecipitations {
+	class Program {
+		static void Main(string[] args) {
+			int[] precipitations = { 23, 45, 4545, 2, -16, 765, 341, 1, -3, 0, 12, 110 };
+
+			// изменяем массив
+			for (int i = 0; i < precipitations.Length; i++) {
+				precipitations[i] = precipitations[i] - precipitations[0];
+			}
+
+			// выводим результат
+			for (int i = 0; i < precipitations.Length; i++) {
+				Console.WriteLine(precipitations[i]);
+			}
+			Console.ReadLine();
+		}
+	}
+}
