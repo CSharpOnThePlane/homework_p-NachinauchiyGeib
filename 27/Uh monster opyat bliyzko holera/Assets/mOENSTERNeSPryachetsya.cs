d85789cfca6df5dc283ed84b86692f1181a7f451
NachinauchiyGeib <superhoroshiy@mail.ru>
2018-05-02 18:19:01 +0300

    3d superhero run
    3d superhero run to princess with traps and monsters and friends

---
+++ 27/Uh monster opyat bliyzko holera/Assets/mOENSTERNeSPryachetsya.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mOENSTERNeSPryachetsya : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform moenster;
+	void Start () {
+		
+	
+	
+	
+	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance(moenster.position, transform.position);	
+		print (dist);
+	
+	
+	
+	}
+}
